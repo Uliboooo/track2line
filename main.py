1a12b3267818f9217d5b8a6cafd7e80ee918c540
yuki <fu08yu20ko07@icloud.com>
2024-04-18 22:17:38 +0900

    変数の整理と冗長なコードの削除

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import os
 import glob
 
+def read_lines_textfile(file_name):
+    data_file = open(f"{file_name}.txt", 'r')
+    return data_file.read()[0:20]
+
 while True:
     print("フォルダのパスを入力してください。")
     folder_path = input()
-
     wav_files = glob.glob(f"{folder_path}/*.wav")
     count = 0
+    renamed_files_path = f"{folder_path}/renamed_files/"
+    os.makedirs(renamed_files_path, exist_ok=True)
 
-    for file in wav_files:
-        before_path = file
-        data_file = open(f"{file.replace(".wav", "")}.txt", 'r')
-        after_name = f"{data_file.read()}"
-        data_file.close()
-        after_name = f"{after_name[0:20]}"
-        last_after_name = after_name[-1]
-        if last_after_name == "/" or last_after_name == "\\":
-            after_name = after_name.replace("/", "")
-        after_name = f"{after_name}.wav"
-        dir = f"{folder_path}/renamed_files/"
-        os.makedirs(dir, exist_ok=True)
-        after_path = f"{dir}{after_name}"
-        os.rename(before_path, after_path)
+    for file in wav_files: #fileは音声ファイルのパス
+        file_name = file.replace(".wav", "")
+        lines = read_lines_textfile(file_name)
+        if lines[-1] == "/":
+            lines = lines.replace("/", "")
+        voice_file_path = f"{renamed_files_path}/{lines}.wav"
+        os.rename(file, voice_file_path)
         count += 1
+
     print(f"{count}個のファイルの名称を変更しました。")
     print("もう一度使用しますか?(y/n)")
     if input() != "y":
