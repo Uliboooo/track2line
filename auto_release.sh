08535c6413e3ab3329e951514389d2107be2873f
coyuki <fu08yu20ko07@icloud.com>
2025-03-27 14:14:21 +0900

    version up t2l lib 0.9.2 -> 0.9.3

--- auto_release.sh
+++ auto_release.sh
@@ -4,12 +4,7 @@
 # いつかpackage nameを取得
 project_name="track2line"
 
-# paths=(
-#     "./target/release/$project_name"
-#     "./target/x86_64-apple-darwin/release/${project_name}"
-#     "./target/x86_64-unknown-linux-gnu/release/${project_name}"
-#     "./target/x86_64-pc-windows-gnu/release/${project_name}.exe"
-# )
+
 
 #build
 cargo fmt
@@ -25,13 +20,12 @@ cargo build --release --target x86_64-pc-windows-gnu
 rm -r release/
 mkdir release/
 
-zip -j -9 ./release/${project_name}_mac_arm.zip ./README.md ./target/release/${project_name}
-zip -j -9 ./release/${project_name}_mac_x86_64.zip ./README.md ./target/x86_64-apple-darwin/release/${project_name}
-zip -j -9 ./release/${project_name}_linux_x86_64.zip ./README.md ./target/x86_64-unknown-linux-gnu/release/${project_name}
-zip -j -9 ./release/${project_name}_win_x86_64.zip ./README.md ./target/x86_64-pc-windows-gnu/release/${project_name}.exe
+cp ./target/release/${project_name} ./release/${project_name}_mac_arm_$1
+cp ./target/x86_64-apple-darwin/release/${project_name} ./release/${project_name}_mac_x86_64_$1
+cp ./target/x86_64-unknown-linux-gnu/release/${project_name} ./release/${project_name}_linux_x86_64_$1
+cp ./target/x86_64-pc-windows-gnu/release/${project_name}.exe ./release/${project_name}_win_x86_64_$1.exe
 
-# for file_path in "${paths[@]}"
-# do
-#     echo ${file_path}
-#     zip x86_64_linux.zip 
-# done
+zip -j -9 ./release/${project_name}_mac_arm.zip ./README.md ./release/${project_name}_mac_arm_$1
+zip -j -9 ./release/${project_name}_mac_x86_64.zip ./README.md ./release/${project_name}_mac_x86_64_$1
+zip -j -9 ./release/${project_name}_linux_x86_64.zip ./README.md ./release/${project_name}_linux_x86_64_$1
+zip -j -9 ./release/${project_name}_win_x86_64.zip ./README.md ./release/${project_name}_win_x86_64_$1.exe
